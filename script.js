ef927d3926e528e1f7a6d42fcf4b702e7c705035
mezbahDev <mezbahuddin9029@gmail.com>
2025-09-08 04:12:20 +0600

    cart section updated

--- script.js
+++ script.js
@@ -16,17 +16,56 @@ const categoryLoadData = () => {
 
 const displayCategory = (posts) => {
   const categoriesList = document.getElementById("categoriesList");
+
   posts.forEach((post) => {
     const a = document.createElement("a");
     a.innerText = post.category_name;
     a.href = "#";
     a.className =
       "px-2 py-1 rounded hover:bg-[#15803D] hover:text-[#FFFFFF] block";
+
+    // click event
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      // add remove active class
+      const allLinks = categoriesList.querySelectorAll("a");
+      allLinks.forEach((link) => {
+        link.classList.remove("bg-[#15803D]", "text-white");
+      });
+
+      a.classList.add("bg-[#15803D]", "text-white");
+
+      loadCategoryPlants(post.id);
+    });
+
     categoriesList.appendChild(a);
+  });
+
+  document.getElementById("allTrees").addEventListener("click", () => {
+    loadAllPlants();
   });
 };
 
 categoryLoadData();
+
+// load plants by category
+const loadCategoryPlants = (categoryId) => {
+  fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      displayCards(data.plants);
+    });
+};
+
+// load All Plants
+const loadAllPlants = () => {
+  fetch("https://openapi.programming-hero.com/api/plants")
+    .then((res) => res.json())
+    .then((data) => {
+      displayCards(data.plants);
+    });
+};
 
 // Tree Card API called
 const cardLoadData = () => {
@@ -66,10 +105,10 @@ const displayCards = (plants) => {
                             <p class="font-semibold text-[14px]">৳ <span>${plant.price}</span></p>
                         </div>
                     </div>
-                    <button class="bg-[#15803D] w-full rounded-[999px] py-[12px] text-white">Add to Cart</button>
+                    <button class="bg-[#15803D] w-full rounded-[999px] py-[12px] text-white hover:bg-green-800">Add to Cart</button>
         `;
-    card.querySelector("button").addEventListener("click",()=>{
-        sum(plant.name, plant.price);
+    card.querySelector("button").addEventListener("click", () => {
+      sum(plant.name, plant.price);
     });
 
     treeCardContainer.appendChild(card);
@@ -86,24 +125,38 @@ const totalPrice = document.getElementById("totalPrice");
 let total = 0;
 
 function sum(name, price) {
-  const item = document.createElement("div");
-  item.className =
-    "flex items-center justify-between bg-[#DCFCE7] px-[12px] py-[8px] rounded-[8px]";
-  item.innerHTML = `
+  const existingItem = Array.from(cartItems.children).find(
+    (item) => item.querySelector("p").innerText === name
+  );
+
+  if (existingItem) {
+    const numberSpan = existingItem.querySelector(".qty");
+    const oldQuantity = parseInt(numberSpan.innerText);
+    numberSpan.innerText = oldQuantity + 1;
+
+    total += price;
+  } else {
+    const item = document.createElement("div");
+    item.className =
+      "flex items-center justify-between bg-[#DCFCE7] px-[12px] py-[8px] rounded-[8px]";
+    item.innerHTML = `
      <div class="flex flex-col gap-[5px]">
                         <p class="tex-[14px] font-semibold">${name}</p>
-                        <p class="text-[#1F2937]">৳ <span id="price">${price}</span> X <span>1</span></p>
+                        <p class="text-[#1F2937]">৳ <span id="price">${price}</span> X <span class="qty">1</span></p>
                     </div>
                     <i id="close" class="fa-solid fa-xmark cursor-pointer"></i>
     `;
-  item.querySelector("i").addEventListener("click", () => {
-    total -= price;
-    updateTotal();
-    item.remove();
-  });
+    item.querySelector("i").addEventListener("click", () => {
+      const itemQuantity = parseInt(item.querySelector(".qty").innerText);
+      const itemTotalPrice = price * itemQuantity;
+      total -= itemTotalPrice;
+      updateTotal();
+      item.remove();
+    });
 
-  cartItems.appendChild(item);
-  total += price;
+    cartItems.appendChild(item);
+    total += price;
+  }
   updateTotal();
 }
 
